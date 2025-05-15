df75b2547e8951a422eb5a974e6e0cfb6397920e
Matteo Grandin <matteo.grandin@igi.cnr.it>
2025-05-15 14:03:24 +0200

    +

--- compile_net_forward.sh
+++ compile_net_forward.sh
@@ -18,18 +18,11 @@ make
 cd ..
 
 # test standalone version
-cp build/net_forward net_forward_standalone
 echo "Testing standalone version..."
 echo "---------------------------------------------------------"
-./net_forward_standalone
+./build/net_forward
 echo "---------------------------------------------------------"
 echo "Standalone version test completed."
-
-echo "Copying mex file to main directory for MATLAB..."
-# Copy the compiled mex files to the current directory
-for file in build/net_forward_mex.mex*; do
-    cp "$file" "net_forward.${file##*.}"
-done
 
 # copy libs to build directory
 cp -r libtorch/lib build/
